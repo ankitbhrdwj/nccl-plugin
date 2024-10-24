e08f50cbefcd86f04a493c0b3d795dbfead8eb03
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2024-10-24 23:14:08 +0000

    Make timeout values to very large (temporarily).

--- src/implement/dpdk/tcp.rs
+++ src/implement/dpdk/tcp.rs
@@ -48,7 +48,7 @@ pub fn libtcp_config(device: &utils::NCCLSocketDev) -> Result<(), Error> {
     #[cfg(not(feature = "storage"))]
     file.write_all(format!("dpdk {{ pci = {}; }}\n", device.pci_path).as_bytes())
         .unwrap();
-    file.write_all(b"tcp { snd_queue_size = 2048; tso = 0; opt_seq = 1; usr_snd_mss = 8192; }\n")
+    file.write_all(b"tcp { snd_queue_size = 2048; tso = 0; opt_seq = 1; usr_snd_mss = 8192; time_wait = 10000000; rto_min = 10000000; }\n")
         .unwrap();
     file.write_all(b"trace { enable = 0; }\n").unwrap();
     file.flush().unwrap();
